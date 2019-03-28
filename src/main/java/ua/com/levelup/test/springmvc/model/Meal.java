b7da7b3b1c9f289f7ea76c854c88f128303d8067
bakteriums <ev.glushenko@gmail.com>
2019-03-28 15:25:31 +0200

    add Scan Premissions Meal and Personal classes.

---
+++ src/main/java/ua/com/levelup/test/springmvc/model/Meal.java
@@ -0,0 +1,20 @@
+package ua.com.levelup.test.springmvc.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "meal")
+public class Meal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Ingridient> ingridients = new ArrayList<Ingridient>();
+    private double ingrigientWeight;
+    private double procentOfTax;
+    private double cost;// Имеет ли смысл хранить сумму сгенерированую единожды из ингридиентов и наценки и пересчет по запросу
+}
